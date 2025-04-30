22af0ff469a930c5943029dcd19f7a7a8f2ffa5e
xxRxxL <icvalimento@gmail.com>
2025-04-30 23:48:43 +0800

    message

--- src/components/inputs/Button.jsx
+++ src/components/inputs/Button.jsx
@@ -4,7 +4,7 @@ export function Button({ children, onClick, className = "" }) {
   return (
     <button
       onClick={onClick}
-      className={`${className} poppins flex-center mt-5 w-42 cursor-pointer rounded-lg px-3 py-5 leading-2.5 lg:w-3/7`}
+      className={`${className} flex-center mt-5 w-42 cursor-pointer rounded-lg px-3 py-5 leading-2.5 lg:w-3/7`}
     >
       {children}
     </button>
